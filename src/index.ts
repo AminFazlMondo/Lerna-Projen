3bd9246bbcdbd8085cbbb60710fe138d46f51553
Amin Fazl <62678026+AminFazlMondo@users.noreply.github.com>
2022-05-24 11:08:55 +1000

    feat(Docs): added document index for sub projects (#128)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {JsonFile, javascript, Project} from 'projen'
+import {JsonFile, javascript, Project, Tasks, SourceCode} from 'projen'
 import {LernaProjectOptions} from './types'
 
 export * from './types'
@@ -6,6 +6,17 @@ export * from './types'
 function getDocsDirectory(project: Project) {
   const result = Object.entries(project).find(([key]) => key === 'docsDirectory')
   return result?.[1].replace(/\/$/, '')
+}
+
+const jsiiTaskPattern = /jsii-docgen -o (?<output>.+)$/i
+
+function extractJsiiDocsOutput(tasks: Tasks): string | undefined {
+  const jsiiDocStep = tasks.tryFind('docgen')?.steps.find(step => jsiiTaskPattern.test(step.exec ?? ''))
+  if (!jsiiDocStep || !jsiiDocStep.exec)
+    return
+
+  const match = jsiiDocStep.exec.match(jsiiTaskPattern)
+  return match?.groups?.output
 }
 
 const lockedTaskNames = ['build', 'upgrade', 'upgrade-projen']
@@ -76,6 +87,7 @@ export class LernaProject extends javascript.NodeProject {
     }))
 
     this.updateSubProjects()
+    this.addDocumentsIndex()
   }
 
   private appendLernaCommands() {
@@ -143,5 +155,47 @@ export class LernaProject extends javascript.NodeProject {
         subBumpTask.builtin('release/reset-version')
       }
     })
+  }
+
+  private addDocumentsIndex() {
+    if (!this.docgen)
+      return
+
+    const subProjectsDocs: Record<string, string> = {}
+
+    const indexMarkdown = new SourceCode(this, `${this.docsDirectory}/index.md`)
+
+    Object.entries(this.subProjects).forEach(([subProjectPath, subProject]) => {
+      const subProjectDocsDirectory = getDocsDirectory(subProject)
+      if (!subProjectDocsDirectory)
+        return
+
+      const subProjectDocsFullPath = `${subProjectPath}/${subProjectDocsDirectory}`
+      const jsiiDocsOutput = extractJsiiDocsOutput(subProject.tasks)
+      if (jsiiDocsOutput) {
+        const path = `${subProjectDocsFullPath}/${jsiiDocsOutput}`
+        indexMarkdown.line(`- ## [${subProject.name}](${path})`)
+        subProjectsDocs[subProject.name] = path
+      } else {
+        subProjectsDocs[subProject.name] = `${subProjectDocsFullPath}/index.html`
+      }
+    })
+
+    const indexHtml = new SourceCode(this, `${this.docsDirectory}/index.html`)
+    indexHtml.line('<!DOCTYPE html>')
+    indexHtml.line('<html>')
+    indexHtml.open('<body>')
+    indexHtml.open('<ul>')
+    Object.entries(subProjectsDocs).forEach(([name, path]) => {
+      indexHtml.open('<li>')
+      indexHtml.open(`<a href="${path}">`)
+      indexHtml.line(name)
+      indexHtml.close('</a>')
+      indexHtml.close('</li>')
+    })
+    indexHtml.close('</ul>')
+    indexHtml.close('</body>')
+    indexHtml.line('</html>')
+    indexHtml.line()
   }
 }
